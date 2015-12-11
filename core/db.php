c267e9e7ac4238eee4b8cef27bba72a6dfe149d6
Edward Lazorskiy <ilovetestosterone@gmail.com>
2015-12-11 13:31:57 +0200

    Db connection

--- core/db.php
+++ core/db.php
@@ -1,7 +1,7 @@
 <?php 
 function get_connection()
 {
-	return mysqli_connect('localhost', 'root', '', 'internetshop');
+	return mysqli_connect('localhost', 'root', '123456', 'internetshop');
 }
 //_____________________________________________________SELECT__________________________________________________________
 function select($table, $where="", $what="")
@@ -36,5 +36,71 @@ function select($table, $where="", $what="")
 	}
 	mysqli_close($db_connection);
 	return $rows;
+}
+//_____________________________________________________INSERT__________________________________________________________
+function insert_user($name, $surname, $phone, $email, $password, $salt)
+{
+	$db_connection=get_connection();
+	$query="INSERT INTO users(name, surname, phone, email, password, salt) VALUES ('$name', '$surname', '$phone', '$email', '$password', '$salt')";
+	mysqli_query($db_connection, $query);
+	mysqli_close($db_connection);
+}
+function insert_product($name, $type, $price, $description, $logo)
+{
+	$db_connection=get_connection();
+	$query="INSERT INTO products(name, type, price, description, logo) VALUES ('$name', '$type', '$price', '$description', '$logo')";
+	mysqli_query($db_connection, $query);
+	mysqli_close($db_connection);
+}
+function insert_images($id, $name)
+{
+	$db_connection=get_connection();
+	$query="INSERT INTO product_gallery(id, name) VALUES ('$id', '$name')";
+	mysqli_query($db_connection, $query);
+	mysqli_close($db_connection);
+}
+//_____________________________________________________DELETE__________________________________________________________
+function delete($sql)
+	{	
+		$db_connection=get_connection();
+		mysqli_query($db_connection,$sql);
+		mysqli_close($db_connection);
+	}
+//_____________________________________________________QUERY__________________________________________________________
+function query($sql)
+	{	
+		$db_connection=get_connection();
+	
+		$rows = mysqli_query($db_connection,$sql);
+		
+		if ($rows) 
+		{	
+			while ($row = mysqli_fetch_assoc($rows))
+			{
+				$result[] = $row;
+			}
+		}
+		else
+		{
+
+			mysqli_error($db_connection);			
+				
+		
+			$result=false;
+		}
+		
+		
+		mysqli_close($db_connection);
+		
+		
+		return $result;
+	}
+//_____________________________________________________INSERT__________________________________________________________
+function update_product($name, $type, $price, $description, $logo, $id)
+{
+	$db_connection=get_connection();
+	$query="UPDATE products SET name='$name', type='$type', price='$price', description='$description', logo='$logo' WHERE id='$id'";
+	mysqli_query($db_connection, $query);
+	mysqli_close($db_connection);
 }
 ?>
