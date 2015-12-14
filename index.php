ed439d056aec89976adb694180afce52016e7b53
serhiynikolskiy <serhiy.nikolskiy@gmail.com>
2015-12-14 18:08:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-include_once("core/core.php");
+/**
+ * Index.php
+ *
+ * PHP Version 5
+ *
+ * @category Ondex
+ * @package  MyPackage
+ * @author   Display Name <username@example.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ */
+require_once "core/core.php";
 load_page();
 ?>
